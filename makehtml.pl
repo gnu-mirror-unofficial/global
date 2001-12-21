c0a2d94e93c605c4d0c5191505e1b0b0ad6deab2
shigio <shigio>
2001-12-21 12:42:57 +0000

    copyright notice added.

--- makehtml.pl
+++ makehtml.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 #
+# Copyright (c) 2001
+#        Tama Communications Corporation. All rights reserved.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
