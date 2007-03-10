8a556a5780759f804f3d02b42b03ba08527a8e0b
shigio <shigio>
2007-03-10 14:33:50 +0000

    Added copyright year.

--- makehtml.pl
+++ makehtml.pl
@@ -32,7 +32,7 @@
 #
 # String
 #
-$l_year = '2000,2001,2002,2003,2004,2005,2006';
+$l_year = '2000,2001,2002,2003,2004,2005,2006, 2007';
 $l_company = 'Tama Communications Corporation';
 $l_name = 'Shigio Yamaguchi';
 $l_copyright = 'Copyright (c)';
