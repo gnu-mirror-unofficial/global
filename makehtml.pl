4f316d7a28ac092705f31dee4ff5d343c9c2a1b4
shigio <shigio>
2012-03-29 22:28:16 +0000

    Modified a link. <a href=http://www.gnu.org/directory/GNU/global.html>GLOBAL</a> | v <a href=http://www.gnu.org/software/global/>GLOBAL</a>

--- makehtml.pl
+++ makehtml.pl
@@ -182,7 +182,7 @@ for ($i = 0; $i < $total; $i++) {
 	print OUTFILE "<!-- $outfile{$infile} generated automatically by makehtml.pl from $infile. -->\n";
 	print OUTFILE "<small>\n";
 	print OUTFILE "<a href=http://www.gnu.org/>GNU Project</a> -\n";
-	print OUTFILE "<a href=http://www.gnu.org/directory/GNU/global.html>GLOBAL</a> / \n";
+	print OUTFILE "<a href=http://www.gnu.org/software/global/>GLOBAL</a> / \n";
 	print OUTFILE "<a href=http://savannah.gnu.org/>Savannah</a> -\n";
 	print OUTFILE "<a href=http://savannah.gnu.org/projects/global/>GLOBAL</a>\n";
 	print OUTFILE "</small>\n";
